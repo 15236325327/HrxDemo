dc82277c051862717588143357ad3903207bb543
HuangRenXiang <2295701930@qq.com>
2020-06-05 13:33:04 +0800

    Initial commit

---
+++ Study_test/src/Arrays/ArraysDemo.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class ArraysDemo {
+
+    public static void main(String[] args) {
+
+        int[] arr={10,20,30,40,50,25,15,35,45};
+
+        Arrays.sort(arr);
+
+        String s = Arrays.toString(arr);
+
+        System.out.println(s);
+    }
+}
---
+++ Study_test/src/duotai/Demo.java
@@ -0,0 +1,21 @@
+package duotai;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+        Animal a=new Cat();
+        a.setName("jia fei");
+        a.setAge(5);
+        System.out.println(a.getName()+":"+a.getAge());
+
+        Cat c=(Cat)a;
+        c.eat();
+        c.playgame();
+
+        a=new Dog("pig",6);
+        System.out.println(a.getName()+a.getAge());
+
+
+    }
+}
